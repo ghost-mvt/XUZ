d5be836fb417b2b54c8e4ed7d1b187feedfc69a8
ghost-mvt <166452924+ghost-mvt@users.noreply.github.com>
2026-05-25 01:53:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
 app.use('/', createProxyMiddleware({
-    target: 'https://www.google.com',
     changeOrigin: true,
     router: (req) => {
         const target = req.url.substring(1);
         return target.startsWith('http') ? target : 'https://' + target;
     },
-    onProxyRes: (proxyRes, req, res) => {
-        // هذه الخطوة هي الأهم: حذف قيود الأمان التي تمنع الـ iframe
+    onProxyRes: (proxyRes) => {
+        // سحر الأمن السيبراني: إزالة قيود الإطارات
         delete proxyRes.headers['x-frame-options'];
         delete proxyRes.headers['content-security-policy'];
+        proxyRes.headers['access-control-allow-origin'] = '*';
     },
     onProxyReq: (proxyReq) => {
         proxyReq.removeHeader('origin');
